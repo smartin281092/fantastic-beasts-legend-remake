af67fedf34249a3af755a67a7215bbaa06a51542
Glitch (glitch-hello-node) <none>
2023-09-19 07:37:35 +0000

    🌾🏗 Checkpoint ./server.js:29993583/8 ./game/challenge.json:29993583/25

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ function createParty() {
   // Read the data.json file to get the list of characters
   const jsonData = fs.readFileSync('game/challenge.json', 'utf-8');
   const data = JSON.parse(jsonData);
-  console.log(data.players);
+  console.log(data);
   
   return data;
 }
