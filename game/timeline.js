d7505afa8a3e9643f682c970de3e0bc38985fe05
Glitch (glitch-hello-node) <none>
2023-09-19 18:51:47 +0000

    📇💐 Checkpoint ./game/timeline.js:29993583/322

--- game/timeline.js
+++ game/timeline.js
@@ -1,13 +1,23 @@
 class Timeline {
   constructor(challenge) {
     this.challenge = challenge;
+    this.timer = 0;
     
-    for()
+    this.roster = [];
+    
+    for(let beast in challenge.playerA.beasts) {
+      this.roster.push(beast);
+    }
+        
+    for(let beast in challenge.playerB.beasts) {
+      this.roster.push(beast);
+    }
+    
     this.sort();
   }
   
   sort() {
-    
+    this.roster((beastA, beastB) => beastB.getSpeed() - beastA.getSpeed());
   }
 }
 
