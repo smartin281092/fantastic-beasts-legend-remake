19e0e5eecbe46c1e9e6a6b7ecb308694aff88450
Glitch (glitch-hello-node) <none>
2023-09-20 09:09:34 +0000

    🚘🍱 Checkpoint ./game/timeline.js:29993583/195

--- game/timeline.js
+++ game/timeline.js
@@ -4,6 +4,7 @@ class Timeline {
     this.timer = 0;
     
     this.roster = [];
+    this.activeBeast;
     
     for(let beastId in challenge.playerA.beasts) {
       this.roster.push(challenge.playerA.beasts[beastId]);
@@ -19,8 +20,17 @@ class Timeline {
   sort() {
     this.roster.sort((beastA, beastB) => {
       return beastA.getSecond() - beastB.getSecond();
-    })
+    });
     console.log('Roster ordered list : ', this.roster[0].getSecond(), this.roster[1].getSecond());
+  }
+  
+  next() {
+    if(this.activeBeast) {
+      this.roster.push(this.activeBeast);
+      this.sort
+    }
+    
+    this.activeBeast = this.roster.shift();
   }
 }
 
