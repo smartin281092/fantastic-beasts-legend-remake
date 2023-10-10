7268243f0dc5607de9deaa80fcf25c1909a40c9f
Glitch (glitch-hello-node) <none>
2023-10-10 13:32:18 +0000

    💡🐘 Checkpoint ./client/my-element.js:29993583/146

--- client/my-element.js
+++ client/my-element.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from "lit";
+import { LitElement, html } from "https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js";
 
 export class MyElement extends LitElement {
   static properties = {
