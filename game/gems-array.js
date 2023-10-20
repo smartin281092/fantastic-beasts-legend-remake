ed4a05aa5250083e3b87baeb9767db746198bc73
Glitch (glitch-hello-node) <none>
2023-10-20 11:57:46 +0000

    🏈💷 Checkpoint ./game/gems-array.js:29993583/231

--- game/gems-array.js
+++ game/gems-array.js
@@ -5,8 +5,12 @@ class GemsArray {
   static width = 6;
   static height = 5;
   static minStreak = 3;
+  static gemColors = 6;
   
   constructor() {
+    this.gameArray = [];
+    this.removeMap = [];
+    this.matchedGems = [];
   }
 }
 
